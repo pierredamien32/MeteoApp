cf472d3b915ce515a9c18c51008d0efe30fd6171
Orphée TOHOU <orpheetohou16@gmail.com>
2023-02-18 23:29:44 +0100

    Mise en place de mes fichires src/app/services/weather.service.ts src/app/app.module.ts src/app/app.component.ts afin de faire la configuration pour l'appelation de l'api de météo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { weatherData } from './models/weather.models';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ProjetAgular';
+export class AppComponent implements OnInit {
+  constructor(private weatherService: WeatherService){
+
+  }
+  
+  cityName: string = 'wellington';
+  weatherData?: weatherData;
+
+  ngOnInit(): void{
+    this.getweatherData(this.cityName);
+  }
+
+  onSubmit(){
+    this.getweatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  public getweatherData(cityName: string){
+    this.weatherService.getWeatherData(cityName)
+    .subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        console.log(response);
+      }
+    }); 
+  }
+  //title = 'ProjetAgular';
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,10 +1,18 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+import { weatherData } from '../models/weather.models';
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
   constructor(private http:HttpClient) { }
+
+  public getWeatherData(cityName: string): Observable<weatherData>{
+    return this.http.get<weatherData>(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=025dfd9f699091b913daa8ce4a3548bb&units=metric&lang=fr`, {
+      headers: new HttpHeaders()
+    });
+  }
 }
