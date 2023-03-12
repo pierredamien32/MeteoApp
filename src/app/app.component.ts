35bee7ab517bd4614df41783be1b4c9c03bab7e9
Orphée TOHOU <orpheetohou16@gmail.com>
2023-03-12 15:35:03 +0100

    modification du fichier src/app/app.component.ts et  src/app/app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from './services/weather.service';
 import { weatherData } from './models/weather.models';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   constructor(private weatherService: WeatherService){
 
   }
   
-  cityName: string = 'wellington';
+  cityName: string = '';
   weatherData?: weatherData;
-
   ngOnInit(): void{
     this.getweatherData(this.cityName);
   }
